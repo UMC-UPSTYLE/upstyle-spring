1ad0881673a1a3433a72b5687a1c65adf364b802
xxinzzi <156905845+xxinzzi@users.noreply.github.com>
2025-01-03 02:05:40 +0900

    Feat: Comment 도메인 구현

---
+++ src/main/java/com/upstyle/upstyle/domain/Comment.java
@@ -0,0 +1,29 @@
+package com.upstyle.upstyle.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Comment {
+    @Id
+    @Column(length = 255)
+    private String id;
+
+    @Column(columnDefinition = "TEXT")
+    private String body;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "ootd_id", nullable = false)
+    private Ootd ootd;
+}
