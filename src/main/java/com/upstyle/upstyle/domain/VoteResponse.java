f571f924a2d2046a3c7ebfc06d45b9610d7846b1
xxinzzi <156905845+xxinzzi@users.noreply.github.com>
2025-01-03 01:43:04 +0900

    Feat: VoteResponse 도메인 구현

---
+++ src/main/java/com/upstyle/upstyle/domain/VoteResponse.java
@@ -0,0 +1,27 @@
+package com.upstyle.upstyle.domain;
+
+import com.upstyle.upstyle.domain.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class VoteResponse extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "option_id", nullable = false)
+    private Option option;
+}
