abf59eba07729fd7577f698afd3b720a37b6e07a
xxinzzi <156905845+xxinzzi@users.noreply.github.com>
2025-01-03 01:44:27 +0900

    Feat: OotdRequest 도메인 구현

---
+++ src/main/java/com/upstyle/upstyle/domain/OotdRequest.java
@@ -0,0 +1,25 @@
+package com.upstyle.upstyle.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class OotdRequest {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String body;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+}
