f91ef14aab9a577efa26bb5ebf3bf8478ae18a24
nonsmileCheetah <pgw31111@gmail.com>
2025-01-07 23:16:54 +0900

    feat: 어노테이션 추가

--- src/main/java/com/upstyle/upstyle/web/controller/UserRestController.java
+++ src/main/java/com/upstyle/upstyle/web/controller/UserRestController.java
@@ -8,12 +8,13 @@ import com.upstyle.upstyle.web.dto.UserRequestDTO;
 import com.upstyle.upstyle.web.dto.UserResponseDTO;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+@Validated
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/users")
