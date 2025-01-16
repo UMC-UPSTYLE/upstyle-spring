c6aa3316e9ccb901c483e741a013813b73c39e8f
xxinzzi <156905845+xxinzzi@users.noreply.github.com>
2025-01-16 18:36:33 +0900

    Feat: 옷 종류에 따른 필터링 조회 기능

--- src/main/java/com/upstyle/upstyle/web/controller/ClothController.java
+++ src/main/java/com/upstyle/upstyle/web/controller/ClothController.java
@@ -28,10 +28,12 @@ public class ClothController {
     @GetMapping("/{kindId}")
     @Operation(summary = "옷 종류에 따른 옷 리스트 조회 API")
     public ApiResponse<ClothResponseDTO.ClothPreviewListDTO> getClothPreviewList(@PathVariable("kindId") Long kindId,
+                                                                                 @RequestParam(value = "userId") Long userId,
                                                                                  @RequestParam(value = "categoryId", required = false) Long categoryId,
-                                                                                 @RequestParam(value = "colorId", required = false) Long colorId,@RequestParam(value = "fitId", required = false) Long fitId) {
-        ClothResponseDTO.ClothPreviewListDTO previewList = new ClothResponseDTO.ClothPreviewListDTO();
+                                                                                 @RequestParam(value = "colorId", required = false) Long colorId,@RequestParam(value = "fitId", required = false) Long fitId, @RequestParam(value = "page", required = false, defaultValue = "0") int page,
+    @RequestParam(value = "size", required = false, defaultValue = "10") int size) {
 
+        ClothResponseDTO.ClothPreviewListDTO previewList = clothQueryService.getClothPreviewList(userId, kindId, categoryId, colorId, fitId, page, size);
         return ApiResponse.onSuccess(previewList);
     }
 }
