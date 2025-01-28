8768efd9d14a0f976d40810ce44178f6425e51cf
xxinzzi <156905845+xxinzzi@users.noreply.github.com>
2025-01-28 18:23:42 +0900

    Feat: closet, cloth 컨트롤러 분리

--- src/main/java/com/upstyle/upstyle/service/ClothService/ClothQueryService.java
+++ src/main/java/com/upstyle/upstyle/service/ClothService/ClothQueryService.java
@@ -4,7 +4,5 @@ import com.upstyle.upstyle.web.dto.ClothResponseDTO;
 
 public interface ClothQueryService {
 
-    ClothResponseDTO.ClothKindListDTO getClothKindList(Long userId);
-
-    ClothResponseDTO.ClothPreviewListDTO getClothPreviewList(Long userId, Long kindId, Long categoryId, Long colorId, Long fitId, int page, int size);
+    ClothResponseDTO.ClothPreviewListDTO getClothPreviewList(Long kindId, Long categoryId, Long colorId, Long fitId, int page, int size);
 }
--- src/main/java/com/upstyle/upstyle/web/controller/ClothController.java
+++ src/main/java/com/upstyle/upstyle/web/controller/ClothController.java
@@ -16,24 +16,14 @@ public class ClothController {
 
     private final ClothQueryService clothQueryService;
 
-    @GetMapping("/")
-    @Operation(summary = "옷장 조회 API")
-    public ApiResponse<ClothResponseDTO.ClothKindListDTO> getClothKindList(@RequestParam(value = "userId") Long userId) {
-        // 서비스 호출
-        ClothResponseDTO.ClothKindListDTO clothKindList = clothQueryService.getClothKindList(userId);
-
-        return ApiResponse.onSuccess(clothKindList);
-    }
-
-    @GetMapping("/{kindId}")
-    @Operation(summary = "옷 종류에 따른 옷 리스트 조회 API")
-    public ApiResponse<ClothResponseDTO.ClothPreviewListDTO> getClothPreviewList(@PathVariable("kindId") Long kindId,
-                                                                                 @RequestParam(value = "userId") Long userId,
+    @GetMapping("/categories")
+    @Operation(summary = "전체 옷 중 카테고리별 조회 API")
+    public ApiResponse<ClothResponseDTO.ClothPreviewListDTO> getClothPreviewList(@RequestParam(value = "kindId", required = false) Long kindId,
                                                                                  @RequestParam(value = "categoryId", required = false) Long categoryId,
                                                                                  @RequestParam(value = "colorId", required = false) Long colorId,@RequestParam(value = "fitId", required = false) Long fitId, @RequestParam(value = "page", required = false, defaultValue = "0") int page,
-    @RequestParam(value = "size", required = false, defaultValue = "10") int size) {
+                                                                                 @RequestParam(value = "size", required = false, defaultValue = "10") int size) {
 
-        ClothResponseDTO.ClothPreviewListDTO previewList = clothQueryService.getClothPreviewList(userId, kindId, categoryId, colorId, fitId, page, size);
+        ClothResponseDTO.ClothPreviewListDTO previewList = clothQueryService.getClothPreviewList(kindId, categoryId, colorId, fitId, page, size);
         return ApiResponse.onSuccess(previewList);
     }
 }
