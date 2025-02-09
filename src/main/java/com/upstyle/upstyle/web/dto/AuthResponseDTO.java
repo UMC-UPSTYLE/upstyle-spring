8d31c6de24b0765f5a64f10ad9e13d0e539fc169
nonsmileCheetah <pgw31111@gmail.com>
2025-02-10 03:56:32 +0900

    feat : kakaoAccessToken추가

--- src/main/java/com/upstyle/upstyle/web/dto/AuthResponseDTO.java
+++ src/main/java/com/upstyle/upstyle/web/dto/AuthResponseDTO.java
@@ -4,11 +4,21 @@ import lombok.*;
 
 public class AuthResponseDTO {
 
-    @Builder
+
     @Getter
-    @NoArgsConstructor
+    @Builder
     @AllArgsConstructor
-    public static class JwtDTO{
+    @NoArgsConstructor
+    public static class JwtDTO {
         private String jwt;
+    }
+
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class KakaoJwtDTO {
+        private String jwt;               // 기존 JWT
+        private String kakaoAccessToken;  // 추가된 카카오 액세스 토큰
     }
 }
